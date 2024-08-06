48491edeecca4bbd96e80384beb04dc4f5152b6b
a1x40v <a1x40v@gmail.com>
2024-08-06 15:12:15 +0300

    Refactor with service

---
+++ src/app/investment.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+
+import { type InvestmentInput } from './investment-input.model';
+import { type InvestmentResult } from './investment-results/investment-result.model';
+
+@Injectable({ providedIn: 'root' })
+export class InvestmentService {
+  resultsData?: InvestmentResult[];
+
+  calculateInvestmentResults(data: InvestmentInput) {
+    const { initialInvestment, annualInvestment, expectedReturn, duration } =
+      data;
+
+    const annualData = [];
+    let investmentValue = initialInvestment;
+
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest =
+        investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+
+    this.resultsData = annualData;
+  }
+}

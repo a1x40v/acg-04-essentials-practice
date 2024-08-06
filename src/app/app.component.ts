d72a9ee1cf54c5bf253f76b94f5459beed34ea0d
a1x40v <a1x40v@gmail.com>
2024-08-06 13:44:46 +0300

    Output data in a table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,19 @@ import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
+import { InvestmentResultsComponent } from './investment-results/investment-results.component';
 import { type InvestmentInput } from './investment-input.model';
+import { type InvestmentResult } from './investment-results/investment-result.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
 })
 export class AppComponent {
+  resultsData?: InvestmentResult[];
+
   onCalculateInvestmentResults(data: InvestmentInput) {
     const { initialInvestment, annualInvestment, expectedReturn, duration } =
       data;
@@ -34,6 +38,6 @@ export class AppComponent {
       });
     }
 
-    console.log(annualData);
+    this.resultsData = annualData;
   }
 }
